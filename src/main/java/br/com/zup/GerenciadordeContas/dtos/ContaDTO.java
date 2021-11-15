9c6fdaff2c5fb626e9e0f446a531cb2c935321e5
Savio <pauloluizsavio@gmail.com>
2021-11-15 13:42:50 -0300

    Criar conta dto

---
+++ src/main/java/br/com/zup/GerenciadordeContas/dtos/ContaDTO.java
@@ -0,0 +1,46 @@
+package br.com.zup.GerenciadordeContas.dtos;
+
+import java.time.LocalDate;
+
+public class ContaDTO {
+
+    private TipoDeConta tipoDeConta;
+    private String nome;
+    private double valor;
+    private LocalDate dataDeVencimento;
+
+    public TipoDeConta getTipoDeConta() {
+        return tipoDeConta;
+    }
+
+    public void setTipoDeConta(TipoDeConta tipoDeConta) {
+        this.tipoDeConta = tipoDeConta;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public LocalDate getDataDeVencimento() {
+        return dataDeVencimento;
+    }
+
+    public void setDataDeVencimento(LocalDate dataDeVencimento) {
+        this.dataDeVencimento = dataDeVencimento;
+    }
+
+    public ContaDTO() {
+    }
+}

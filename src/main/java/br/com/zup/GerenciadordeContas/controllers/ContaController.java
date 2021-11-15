015179d8fa87de93a9218591c167299bffd97723
Savio <pauloluizsavio@gmail.com>
2021-11-15 17:30:07 -0300

    Criar conta controller

---
+++ src/main/java/br/com/zup/GerenciadordeContas/controllers/ContaController.java
@@ -0,0 +1,11 @@
+package br.com.zup.GerenciadordeContas.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/contas")
+public class ContaController {
+
+
+}
